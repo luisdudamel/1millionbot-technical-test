c3cb55625f7be2cc4a55fa8ce185e37e2309a8df
Luis Dudamel <ladusa@gmail.com>
2024-06-05 23:23:54 +0200

    Add cleanup function after each test for vitest configuration

--- src/components/SendButton/SendButton.test.ts
+++ src/components/SendButton/SendButton.test.ts
@@ -5,17 +5,19 @@ import SendButton from "./SendButton.vue"
 describe("Given a SendButton component", () => {
   const expectedAriaLabel = "Send"
 
-  const { getByRole, emitted } = render(SendButton)
-  const sendButton = getByRole("button", { name: expectedAriaLabel })
-
   describe("When it's mounted", () => {
     test("Then it should render a button with the accesible name `Send`", () => {
+      const { getByRole } = render(SendButton)
+      const sendButton = getByRole("button", { name: expectedAriaLabel })
       expect(sendButton).not.toBeNull()
     })
   })
 
   describe("When it's mounted and the user clicks on the button with the accesible name `Send`", () => {
     test("Then it should emit the 'send-message' event", async () => {
+      const { getByRole, emitted } = render(SendButton)
+      const sendButton = getByRole("button", { name: expectedAriaLabel })
+
       await fireEvent.click(sendButton)
 
       expect(emitted()).toHaveProperty(["send-message"])
